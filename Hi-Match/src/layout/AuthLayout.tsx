a374b8edd6b81a14669071accb2fa3b8f6369de1
WHOOZ-23 <riahboa@naver.com>
2025-05-13 01:21:15 +0900

    :lipstick:style: 로고 변경

--- Hi-Match/src/layout/AuthLayout.tsx
+++ Hi-Match/src/layout/AuthLayout.tsx
@@ -1,12 +1,12 @@
 import { Link, Outlet } from "react-router-dom";
-import logo from "@/assets/images/header/logo-header.svg";
+import LogoIcon from "@/assets/images/header/logo-header.svg?react";
 
 const AuthLayout = () => {
     return (
         <div className="grid-center min-h-screen space-y-5 pt-25 pb-50">
             <header className="logo">
                 <Link to="/">
-                    <img src={logo} alt="Hi Match" />
+                    <LogoIcon className="w-50" />
                 </Link>
             </header>
             <main className="min-w-87">
