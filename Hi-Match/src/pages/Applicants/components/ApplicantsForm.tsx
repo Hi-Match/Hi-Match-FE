b9cee2fe27212b54c8984c4da06ff3ec123d1e9f
miseullang <miseullang@gmail.com>
2025-05-15 03:37:48 +0900

    :lipstick:style: 지원자 관리 및 채용 공고 관리 모달 기본값 설정

--- Hi-Match/src/pages/Applicants/components/ApplicantsForm.tsx
+++ Hi-Match/src/pages/Applicants/components/ApplicantsForm.tsx
@@ -6,7 +6,7 @@ const ApplicantsForm = () => {
     const [selectedPostNo, setSelectedPostNo] = useState<number | null>(null);
 
     return (
-        <div className="w-full space-y-12.5 rounded-[10px] border border-solid border-gray-50 bg-white p-12.5 shadow-sm">
+        <div className="mx-auto flex w-full max-w-[1000px] min-w-[1000px] flex-col gap-16 space-y-12.5 rounded-[10px] border-1 border-solid border-gray-50 bg-white p-14 shadow-sm min-h-[80vh]">
             <RecruitFormList onSelectPost={setSelectedPostNo} />
             <ApplicantsList postNo={selectedPostNo} />
         </div>
